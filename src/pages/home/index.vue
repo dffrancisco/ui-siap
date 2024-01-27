ed9f342942c82803a39ea4fd2521b55d3f18ff0e
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-01-27 17:28:34 -0300

    feat: add rota home descontoCliente

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -75,6 +75,12 @@ import login from "../login/login";
 
             <v-btn
               class="mr-4 mb-4"
+              @click="router.push('/descontoCliente')"
+              >Desconto Cliente
+            </v-btn>
+
+            <v-btn
+              class="mr-4 mb-4"
               @click="login.actions.confirmarSaida()"
               >Logout
             </v-btn>
