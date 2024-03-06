e337cdb90b4c22fe4bb5b87a82f8562c58d7b262
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-03-06 15:03:06 -0300

    feat: add datatable

--- src/pages/vendaPorMarca/index.vue
+++ src/pages/vendaPorMarca/index.vue
@@ -1,1 +1,14 @@
-<template> VENDA POR MARCA </template>
+<script setup lang="ts">
+import { state } from "./vendaPorMarca";
+</script>
+
+<template>
+  <v-container>
+    <v-card>
+      <v-data-table
+        :headers="state.headers"
+        :items="state.itens"
+      ></v-data-table>
+    </v-card>
+  </v-container>
+</template>
---
+++ src/pages/vendaPorMarca/vendaPorMarca.ts
@@ -0,0 +1,27 @@
+import utils from "@/ts/utils";
+import { reactive } from "vue";
+
+export const state = reactive({
+
+    headers: <any>[
+        { title: 'Marcas', key: 'DESCRICAO', align: 'center', },
+        { title: 'Valor', key: 'VALOR', align: 'center' },
+        { title: 'Qtd. Itens', key: 'QTD', align: 'center' },
+        { title: 'Ticket Médio', key: 'TICKET_MEDIO', align: 'center' },
+        { title: 'Qtd. Média Itens', key: 'QTD_MEDIA_ITENS', align: 'center' },
+        { title: 'Percentual', key: 'PERCENTUAL', align: 'center' },
+    ],
+
+    itens: [
+        {
+            DESCRICAO: 'DTS',
+            VALOR: utils.formatValor('20846.90'),
+            QTD: 123,
+            TICKET_MEDIO: utils.formatValor('169.48'),
+            QTD_MEDIA_ITENS: 1,
+            PERCENTUAL: '30%'
+        },
+    ]
+})
+
+export default { state }
