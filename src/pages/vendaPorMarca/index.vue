016bfcac4813f9f67e9c3e327c410c638313781d
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-03-06 15:56:01 -0300

    feat: add text fields and select

--- src/pages/vendaPorMarca/index.vue
+++ src/pages/vendaPorMarca/index.vue
@@ -5,6 +5,28 @@ import { state } from "./vendaPorMarca";
 <template>
   <v-container>
     <v-card>
+      <v-row class="pa-5">
+        <v-col cols="4">
+          <v-autocomplete
+            id="mes"
+            label="Mês"
+            item-title="title"
+            item-value="value"
+            autocomplete="off"
+          ></v-autocomplete>
+        </v-col>
+        <v-col cols="4">
+          <v-text-field
+            label="Ano"
+            class="input_ano"
+            variant="filled"
+          ></v-text-field>
+        </v-col>
+        <v-col class="ml-9 mt-4"
+          ><v-btn color="primary mr-5"> <v-icon class="mr-2">mdi-magnify </v-icon>Consultar </v-btn>
+          <v-btn color="primary"> <v-icon class="mr-2">mdi-printer </v-icon>Imprimir </v-btn>
+        </v-col>
+      </v-row>
       <v-data-table
         :headers="state.headers"
         :items="state.itens"
@@ -12,3 +34,9 @@ import { state } from "./vendaPorMarca";
     </v-card>
   </v-container>
 </template>
+
+<style scoped>
+.input_ano {
+  height: 94px;
+}
+</style>
--- src/pages/vendaPorMarca/vendaPorMarca.ts
+++ src/pages/vendaPorMarca/vendaPorMarca.ts
@@ -4,17 +4,17 @@ import { reactive } from "vue";
 export const state = reactive({
 
     headers: <any>[
-        { title: 'Marcas', key: 'DESCRICAO', align: 'center', },
+        { title: 'Marcas', key: 'DESCRICAO', align: 'center', width: '30%' },
         { title: 'Valor', key: 'VALOR', align: 'center' },
         { title: 'Qtd. Itens', key: 'QTD', align: 'center' },
-        { title: 'Ticket Médio', key: 'TICKET_MEDIO', align: 'center' },
         { title: 'Qtd. Média Itens', key: 'QTD_MEDIA_ITENS', align: 'center' },
+        { title: 'Ticket Médio', key: 'TICKET_MEDIO', align: 'center' },
         { title: 'Percentual', key: 'PERCENTUAL', align: 'center' },
     ],
 
     itens: [
         {
-            DESCRICAO: 'DTS',
+            DESCRICAO: 'IMPORTADO TONG YANG',
             VALOR: utils.formatValor('20846.90'),
             QTD: 123,
             TICKET_MEDIO: utils.formatValor('169.48'),
