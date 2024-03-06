bb08802924ad784022ea1f62c0ba537599e469de
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-03-06 16:21:13 -0300

    fix: ajustes

--- src/pages/gerenciarFolhaPontoDetalhes/services/gerenciarFolhaPontoDetalhes.service.ts
+++ src/pages/gerenciarFolhaPontoDetalhes/services/gerenciarFolhaPontoDetalhes.service.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
 import {
-  iDadosDocumento,
-  iDeletarArquivo,
-  iDeletarDocumento,
-  iDeletarFalta,
-  iDeleteArquivoResponse,
-  iDeleteDocumentoResponse,
-  iDeleteFaltaResponse,
-  iFaltaFeriadoFolga,
-  iGetDadosParaImpressaoIndividual,
-  iGetDadosParaImpressaoPDFResponse,
-  iGetDetalhes,
-  iGetDetalhesResponse,
-  iInserirRegistroAusenciaResponse,
-  iParamDocumentoAusencia,
-  iRegistrarDocumentoAusencia,
-  iRegistrarFalta,
-  iSetFaltaResponse,
-  iSetFeriadoFolgaResponse,
+    iDadosDocumento,
+    iDeletarArquivo,
+    iDeletarDocumento,
+    iDeletarFalta,
+    iDeleteArquivoResponse,
+    iDeleteDocumentoResponse,
+    iDeleteFaltaResponse,
+    iFaltaFeriadoFolga,
+    iGetDadosParaImpressaoIndividual,
+    iGetDadosParaImpressaoPDFResponse,
+    iGetDetalhes,
+    iGetDetalhesResponse,
+    iInserirRegistroAusenciaResponse,
+    iParamDocumentoAusencia,
+    iRegistrarDocumentoAusencia,
+    iRegistrarFalta,
+    iSetFaltaResponse,
+    iSetFeriadoFolgaResponse,
 } from "../interface";
 const caminho = "siap/gerenciarFolhaPonto";
 
@@ -30,169 +30,166 @@ type iDeleteArquivo = (param: iDeletarArquivo) => Promise<iDeleteArquivoResponse
 type iInserirRegistroAusencia = (param: iRegistrarDocumentoAusencia) => Promise<iInserirRegistroAusenciaResponse>;
 type iGetDocumentoAusencia = (param: iParamDocumentoAusencia) => Promise<iDadosDocumento[]>;
 type iGetDadosParaImpressaoPDF = (
-  param: iGetDadosParaImpressaoIndividual
+    param: iGetDadosParaImpressaoIndividual
 ) => Promise<iGetDadosParaImpressaoPDFResponse>;
 
 const getDetalhes: igetDetalhes = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "getDetalhes",
-    param,
-  });
-  return data;
+    let { data } = await axios.post(caminho, {
+        call: "getDetalhes",
+        param,
+    });
+    return data;
 };
 
 const setFaltaFeriadoOuFolga: iSetFeriadoFolga = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "setFaltaFeriadoOuFolga",
-    param,
-  });
+    let { data } = await axios.post(caminho, {
+        call: "setFaltaFeriadoOuFolga",
+        param,
+    });
 
-  return data;
+    return data;
 };
 
 const setFalta: iSetFalta = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "setFalta",
-    param,
-  });
+    let { data } = await axios.post(caminho, {
+        call: "setFalta",
+        param,
+    });
 
-  return data;
+    return data;
 };
 
 const deletarFalta: iDeleteFalta = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "deletarFalta",
-    param,
-  });
+    let { data } = await axios.post(caminho, {
+        call: "deletarFalta",
+        param,
+    });
 
-  return data;
+    return data;
 };
 
 const deletarDocumento: iDeleteDocumento = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "deletarDocumento",
-    param,
-  });
+    let { data } = await axios.post(caminho, {
+        call: "deletarDocumento",
+        param,
+    });
 
-  return data;
+    return data;
 };
 
 const deletarArquivo: iDeleteArquivo = async (param) => {
-  return $.ajax({
-    url: "http://reallatas.com.br/doc_funcionario/getFiles.php",
-    type: "POST",
-    dataType: "json",
-    data: {
-      call: "deleteArquivo",
-      class: "Files",
-      param: {
-        cpf: param.cpf,
-        folderName: param.folderName,
-        nomeArquivo: param.file_name,
-        usuario: param.usuario,
-      },
-    },
-  });
+    return $.ajax({
+        url: "http://reallatas.com.br/doc_funcionario/getFiles.php",
+        type: "POST",
+        dataType: "json",
+        data: {
+            call: "deleteArquivo",
+            class: "Files",
+            param: {
+                cpf: param.cpf,
+                folderName: param.folderName,
+                nomeArquivo: param.file_name,
+                usuario: param.usuario,
+            },
+        },
+    });
 };
 
 const uploadPDF = async (formData) => {
-  try {
-    const rs = await $.ajax({
-      url: "http://www.reallatas.com.br/doc_funcionario/getFiles.php",
-      type: "POST",
-      data: formData,
-      processData: false,
-      contentType: false,
-    });
-    return rs;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const rs = await $.ajax({
+            url: "http://www.reallatas.com.br/doc_funcionario/getFiles.php",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+        });
+        return rs;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const verificarArquivos = async (cpf) => {
-  try {
-    const rs = await $.ajax({
-      url: "https://reallatas.com.br/doc_funcionario/getFiles.php",
-      type: "POST",
-      dataType: "json",
-      data: {
-        class: "Files",
-        call: "getFilesTemp",
-        param: {
-          cpf: cpf,
-          pasta: "ausencia",
-        },
-      },
-    });
-    return rs;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const rs = await $.ajax({
+            url: "https://reallatas.com.br/doc_funcionario/getFiles.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                class: "Files",
+                call: "getFilesTemp",
+                param: {
+                    cpf: cpf,
+                    pasta: "ausencia",
+                },
+            },
+        });
+        return rs;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const moverArquivoTemp = async (tipoDocumento, cpf) => {
-  try {
-    const rs = await $.ajax({
-      url: "https://reallatas.com.br/doc_funcionario/getFiles.php",
-      type: "POST",
-      dataType: "json",
-      data: {
-        class: "Files",
-        call: "moverArquivoTemp",
-        param: {
-          cpf: cpf,
-          pasta: tipoDocumento,
-        },
-      },
-    });
-    return rs;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const rs = await $.ajax({
+            url: "https://reallatas.com.br/doc_funcionario/getFiles.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                class: "Files",
+                call: "moverArquivoTemp",
+                param: {
+                    cpf: cpf,
+                    pasta: tipoDocumento,
+                },
+            },
+        });
+        return rs;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const createRegistroDocumento: iInserirRegistroAusencia = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "createRegistroDocumento",
-    param,
-  });
+    let { data } = await axios.post(caminho, {
+        call: "createRegistroDocumento",
+        param,
+    });
 
-  return data;
+    return data;
 };
 
 const getDocumentoAusencia: iGetDocumentoAusencia = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "getDocumentoAusencia",
-    param,
-  });
+    let { data } = await axios.post(caminho, {
+        call: "getDocumentoAusencia",
+        param,
+    });
 
-  return data;
+    return data;
 };
 
 const getDadosParaImpressao: iGetDadosParaImpressaoPDF = async (param) => {
-  let { data } = await axios.post(caminho, {
-    call: "getDadosParaImpressao",
-    param,
-  });
+    let { data } = await axios.post(caminho, {
+        call: "getDadosParaImpressao",
+        param,
+    });
 
-  return data;
+    return data;
 };
 
 export default {
-  getDetalhes,
-  // getPontos,
-  // getResumoPontosFuncionario,
-  // getTipoFaltas,
-  setFaltaFeriadoOuFolga,
-  deletarFalta,
-  deletarDocumento,
-  deletarArquivo,
-  createRegistroDocumento,
-  setFalta,
-  getDocumentoAusencia,
-  uploadPDF,
-  verificarArquivos,
-  moverArquivoTemp,
-  getDadosParaImpressao,
+    getDetalhes,
+    setFaltaFeriadoOuFolga,
+    deletarFalta,
+    deletarDocumento,
+    deletarArquivo,
+    createRegistroDocumento,
+    setFalta,
+    getDocumentoAusencia,
+    uploadPDF,
+    verificarArquivos,
+    moverArquivoTemp,
+    getDadosParaImpressao,
 };
--- src/router.ts
+++ src/router.ts
@@ -6,29 +6,36 @@ import storeLogin from "./pages/login/login";
 import routes from "virtual:generated-pages";
 import axios from "axios";
 
+
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHashHistory(),
+    routes,
 });
 
-// console.log(routes);
+console.log(routes);
 
 router.beforeEach((to, from, next) => {
-  // console.log(to);
 
-  if (to.query.chave != undefined) {
-    // console.log(to.query.chave);
-    sessionStorage.setItem(storeLogin.state.nameStorage, JSON.stringify({ token: to.query.chave }));
-    storeLogin.state.auth == true;
-    axios.defaults.headers.common["Authorization"] = to.query.chave;
-    storeLogin.actions.setAuth(true);
-    next();
-  } else {
-    auth();
-    if (to.name !== "login" && storeLogin.state.auth == false) next({ path: "login" });
-    else if (to.name == "login" && storeLogin.state.auth == true) next({ path: "home" });
-    else next();
-  }
+    // console.log(to);
+
+    if (to.query.chave != undefined) {
+        // console.log(to.query.chave);
+        sessionStorage.setItem(
+            storeLogin.state.nameStorage,
+            JSON.stringify({ token: to.query.chave })
+        );
+        storeLogin.state.auth == true
+        axios.defaults.headers.common["Authorization"] = to.query.chave;
+        storeLogin.actions.setAuth(true);
+        next();
+    } else {
+        auth();
+        if (to.name !== "login" && storeLogin.state.auth == false)
+            next({ path: "login" });
+        else if (to.name == "login" && storeLogin.state.auth == true)
+            next({ path: "home" });
+        else next();
+    }
 });
 
 export default router;
