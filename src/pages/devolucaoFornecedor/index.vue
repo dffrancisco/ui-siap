b0e9d89fca62f7dec74a1d645579f2679472cc86
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-04-05 09:41:12 -0300

    fix: size

--- src/pages/devolucaoFornecedor/index.vue
+++ src/pages/devolucaoFornecedor/index.vue
@@ -58,7 +58,7 @@ onUnmounted(() => {
       class="pa-4"
       style="margin: 0 auto"
       width="1164px"
-      height="728px"
+      height="692"
     >
       <div class="btns">
         <v-btn
@@ -76,10 +76,10 @@ onUnmounted(() => {
           Nova devolução (F2)
         </v-btn>
       </div>
-      <div class="pt-5">
+      <div class="pt-4">
         <v-row>
           <v-col cols="7">
-            <h2 class="pb-2 font-weight-regular">Dados do Fornecedor</h2>
+            <h2 class="pb-1 font-weight-regular">Dados do Fornecedor</h2>
             <div class="container"
               ><v-row>
                 <v-col cols="3">
@@ -94,14 +94,14 @@ onUnmounted(() => {
             </div>
           </v-col>
           <v-col>
-            <h2 class="pb-2 font-weight-regular">Notas vinculadas ({{ notasAgrupadas.length }})</h2>
+            <h2 class="pb-1 font-weight-regular">Notas vinculadas ({{ notasAgrupadas.length }})</h2>
             <div class="container notas_vinculadas">
               <span v-if="notasAgrupadas.length == 0">Nenhuma nota vinculada, necessário adicionar itens!</span>
               <v-card
                 v-if="state.dbDevolucao.ID_DEVOLUCAO_FORNECEDOR"
                 v-for="notas in notasAgrupadas"
               >
-                <div class="card_nota pa-2">
+                <div class="card_nota pa-1">
                   <span>{{ notas.NUM_NOTA }}</span>
                   <p>{{ utils.dataBrasil(notas.DATA_EMISSAO) }}</p>
                 </div>
@@ -110,8 +110,8 @@ onUnmounted(() => {
           </v-col>
         </v-row>
       </div>
-      <div class="pt-5">
-        <h2 class="pb-2 font-weight-regular">Dados da Transportadora</h2>
+      <div class="pt-4">
+        <h2 class="pb-1 font-weight-regular">Dados da Transportadora</h2>
         <div class="container">
           <v-row>
             <v-col cols="6">
@@ -144,8 +144,8 @@ onUnmounted(() => {
       </div>
 
       <!-- componente dados da devolucao -->
-      <div class="pt-5">
-        <h2 class="pb-2 font-weight-regular">Dados da Devolução</h2>
+      <div class="pt-4">
+        <h2 class="pb-1 font-weight-regular">Dados da Devolução</h2>
         <div class="container">
           <v-row>
             <v-col cols="2">
@@ -168,10 +168,10 @@ onUnmounted(() => {
         </div>
       </div>
       <div
-        class="pt-5"
+        class="pt-4"
         style="min-height: 210px"
       >
-        <h2 class="pb-2 font-weight-regular">Itens</h2>
+        <h2 class="pb-1 font-weight-regular">Itens</h2>
         <div class="cards">
           <v-card
             v-if="state.dbDevolucao.STATUS != 1"
@@ -211,7 +211,7 @@ onUnmounted(() => {
         </div>
       </div>
 
-      <div class="btns pt-5">
+      <div class="btns pt-4">
         <div class="d-flex flex-wrap ga-2">
           <v-btn
             color="#3680AB"
