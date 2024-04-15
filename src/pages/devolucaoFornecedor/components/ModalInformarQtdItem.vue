bf3c0b01c786ec1a24d53794c928683381e67f5c
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-04-15 17:16:06 -0300

    Revert "fix: pis cst invalido"
    This reverts commit b34302171a42b5f7e1abf7504c0180521bac4171.

--- src/pages/devolucaoFornecedor/components/ModalInformarQtdItem.vue
+++ src/pages/devolucaoFornecedor/components/ModalInformarQtdItem.vue
@@ -48,7 +48,7 @@ watch(
         BASE_ICMS_ST: props.dbItem.BASE_ICMS_ST,
         PERCENTUAL_ICMS: props.dbItem.PERCENTUAL_ICMS,
         PERCENTUAL_IPI: utils.formatValor(props.dbItem.PERCENTUAL_IPI) || 0,
-        CST_PIS: props.dbItem.CST_PIS || "",
+        CST_PIS: props.dbItem.CST_PIS,
         PERCENTUAL_PIS: utils.formatValor(props.dbItem.PERCENTUAL_PIS) || 0,
         CST_COFINS: props.dbItem.CST_COFINS,
         PERCENTUAL_COFINS: utils.formatValor(props.dbItem.PERCENTUAL_COFINS) || 0,
@@ -60,18 +60,15 @@ watch(
 
       await actions.getTributosPisCofinsItem();
 
-      actions.validarCstIpi();
-
       state.edtItemQtd.focus();
     }
   }
 );
 
-const ipiCstComPercentual = ["50", "99"];
-const ipiCstValido = ["50", "51", "52", "53", "54", "55", "99"];
+const ipiCst = ["50", "99"];
 
 const disabledPercentualIPI = computed(() => {
-  return ipiCstComPercentual.includes(state.dbItemDevolucao.CST_IPI) ? false : true;
+  return ipiCst.includes(state.dbItemDevolucao.CST_IPI) ? false : true;
 });
 
 const state = reactive({
@@ -103,14 +100,8 @@ const actions = {
     actions.salvarItemDevolucao();
   },
 
-  validarCstIpi() {
-    if (!ipiCstValido.includes(state.dbItemDevolucao.CST_IPI)) {
-      state.dbItemDevolucao.CST_IPI = props.dbItem.CST_IPI || "";
-    }
-  },
-
   definirPercentualIPIComBaseNoCST() {
-    if (ipiCstComPercentual.includes(state.dbItemDevolucao.CST_IPI)) {
+    if (ipiCst.includes(state.dbItemDevolucao.CST_IPI)) {
       state.dbItemDevolucao.PERCENTUAL_IPI = utils.formatValor(props.dbItem.PERCENTUAL_IPI);
     } else {
       state.dbItemDevolucao.PERCENTUAL_IPI = 0;
@@ -199,7 +190,7 @@ const actions = {
     }
 
     if (
-      ipiCstComPercentual.includes(state.dbItemDevolucao.CST_IPI) &&
+      ipiCst.includes(state.dbItemDevolucao.CST_IPI) &&
       utils.formatValorUSA(state.dbItemDevolucao.PERCENTUAL_IPI.toString()) <= 0
     ) {
       await Swal.fire({
