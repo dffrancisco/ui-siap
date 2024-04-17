68092408acf6ad82ab242d77ffba3f6acb80e44b
guigagb <bsbtrindade@gmail.com>
2024-04-17 09:55:46 -0300

    ajuste para consumir dados

--- src/pages/comprasItens/components/Cabecalho.vue
+++ src/pages/comprasItens/components/Cabecalho.vue
@@ -12,47 +12,50 @@ const props = defineProps({
 
 <template>
   <div class="cabecalho">
-    <div>
+    <div class="cabecalho-dados">
       <div>
-        <span class="mr-1">Pedido:</span>
-        <strong>{{ props.cabecalho.ID_COMPRAS }}</strong>
+        <div>
+          <span class="mr-1">Pedido:</span>
+          <strong>{{ props.cabecalho.ID_COMPRAS }}</strong>
+        </div>
+        <div class="mt-2">
+          <span class="mr-1">Data:</span>
+          <strong>{{ utils.dataBrasil(props.cabecalho.DATA) }}</strong>
+        </div>
       </div>
-      <div class="mt-2">
-        <span class="mr-1">Data:</span>
-        <strong>{{ utils.dataBrasil(props.cabecalho.DATA) }}</strong>
-      </div>
-    </div>
-    <div>
       <div>
-        <span class="mr-1">Qtd Itens:</span>
-        <strong>{{ props.cabecalho.QTD_ITENS }}</strong>
+        <div>
+          <span class="mr-1">Qtd Itens:</span>
+          <strong>{{ props.cabecalho.QTD_ITENS }}</strong>
+        </div>
+        <div class="mt-2">
+          <span class="mr-1">Valor:</span>
+          <strong>{{ utils.formatValor(props.cabecalho.VALOR) }}</strong>
+        </div>
       </div>
-      <div class="mt-2">
-        <span class="mr-1">Valor:</span>
-        <strong>{{ utils.formatValor(props.cabecalho.VALOR) }}</strong>
-      </div>
-    </div>
-    <div>
       <div>
-        <span class="mr-1">Comprador:</span>
-        <strong>{{ props.cabecalho.COMPRADOR }}</strong>
-      </div>
-      <div class="mt-2">
-        <span class="mr-1">Marca:</span>
-        <v-chip
-          variant="outlined"
-          color="blue"
-          size="x-small"
-          class="px-2 mr-1"
-          >{{ props.cabecalho.MARCA_PRINCIPAL }}</v-chip
-        >
-        <v-chip
-          variant="outlined"
-          color="blue"
-          size="x-small"
-          class="px-2"
-          >+{{ props.cabecalho.MARCAS.length }}</v-chip
-        >
+        <div>
+          <span class="mr-1">Comprador:</span>
+          <strong>{{ props.cabecalho.COMPRADOR }}</strong>
+        </div>
+        <div class="mt-2">
+          <span class="mr-1">Marca:</span>
+          <v-chip
+            variant="outlined"
+            color="blue"
+            size="x-small"
+            class="px-2 mr-1"
+            >{{ props.cabecalho.NOME_MARCA }}</v-chip
+          >
+          <v-chip
+            v-if="props.cabecalho?.MARCAS?.length > 0"
+            variant="outlined"
+            color="blue"
+            size="x-small"
+            class="px-2"
+            >+{{ props.cabecalho?.MARCAS?.length }}</v-chip
+          >
+        </div>
       </div>
     </div>
     <div class="cabecalho-obs">
@@ -87,6 +90,12 @@ const props = defineProps({
   display: flex;
   gap: 32px;
   height: 72px;
+}
+
+.cabecalho-dados {
+  width: 50%;
+  display: flex;
+  gap: 32px;
 }
 
 .cabecalho-obs {
