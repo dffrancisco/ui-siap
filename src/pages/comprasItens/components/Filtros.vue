31fd503e91f79bdda5f31d6eaabfcde3e8c94406
guigagb <bsbtrindade@gmail.com>
2024-04-17 09:56:05 -0300

    implementar logica dos filtros

--- src/pages/comprasItens/components/Filtros.vue
+++ src/pages/comprasItens/components/Filtros.vue
@@ -1,38 +1,131 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, watch } from "vue";
+import { iCarro, iMarca } from "../interfaces";
+
+const props = defineProps({
+  idMarcaInicial: {
+    type: Number,
+    default: undefined,
+  },
+  carros: {
+    type: Array as () => iCarro[],
+    required: true,
+  },
+  marcas: {
+    type: Array as () => iMarca[],
+    required: true,
+  },
+});
+
+const emit = defineEmits(["buscarProdutos"]);
+
+const invertalBusca = setInterval(() => actions.callEmitBuscarProdutos, 1000);
+
+const state = reactive({
+  edtNumFabricante: undefined,
+  edtDescricao: undefined,
+  edtCarro: undefined,
+  edtMarca: undefined,
+  tempoNovaBusca: 1000,
+});
+
+const actions = {
+  callEmitBuscarProdutos: () => {
+    clearInterval(stopTime);
+
+    emit("buscarProdutos", {
+      ID_MARCA: state.edtMarca,
+      ID_CARRO: state.edtCarro,
+      DESC_PRODUTO: state.edtDescricao,
+      NUM_FABRICANTE: state.edtNumFabricante,
+    });
+  },
+};
+
+watch(
+  () => props.idMarcaInicial,
+  () => {
+    if (props.idMarcaInicial) {
+      state.edtMarca = props.idMarcaInicial;
+    }
+  }
+);
+
+let stopTime;
+
+watch(
+  () => [state.edtNumFabricante, state.edtDescricao],
+  ([newNumFabricante, newDescricao], [oldNumFabricante, oldDescricao]) => {
+    clearInterval(stopTime);
+
+    stopTime = setInterval(() => {
+      state.tempoNovaBusca = state.tempoNovaBusca - 100;
+
+      if (state.tempoNovaBusca == 0) {
+        state.tempoNovaBusca = 1000;
+
+        if (newNumFabricante != oldNumFabricante) {
+          actions.callEmitBuscarProdutos();
+          return;
+        }
+
+        if (newDescricao != oldDescricao) {
+          actions.callEmitBuscarProdutos();
+        }
+      }
+    }, 100);
+  }
+);
+</script>
 
 <template>
   <div class="filtros">
     <div class="filtros-fabricante">
       <label>Nº Fabricante</label>
       <v-text-field
+        v-model="state.edtNumFabricante"
         variant="outlined"
         density="compact"
         bg-color="#3b4758"
+        @keypress.enter="actions.callEmitBuscarProdutos"
       ></v-text-field>
     </div>
     <div class="filtros-descricao">
       <label>Descrição</label>
       <v-text-field
+        v-model="state.edtDescricao"
         variant="outlined"
         density="compact"
         bg-color="#3b4758"
+        @keypress.enter="actions.callEmitBuscarProdutos"
       ></v-text-field>
     </div>
     <div class="filtros-carro">
       <label>Carro</label>
-      <v-select
+      <v-autocomplete
+        v-model="state.edtCarro"
         variant="outlined"
         density="compact"
         bg-color="#3b4758"
-      ></v-select>
+        :items="props.carros"
+        item-value="ID_CARRO"
+        item-title="NOME_CARRO"
+        @update:model-value="actions.callEmitBuscarProdutos"
+      ></v-autocomplete>
     </div>
     <div class="filtros-marca">
       <label>Marca</label>
-      <v-select
+      <v-autocomplete
+        v-model="state.edtMarca"
         variant="outlined"
         density="compact"
         bg-color="#3b4758"
-      ></v-select>
+        :items="props.marcas"
+        item-value="ID_MARCA"
+        item-title="NOME_MARCA"
+        :clearable="false"
+        @update:model-value="actions.callEmitBuscarProdutos"
+      ></v-autocomplete>
     </div>
     <div class="d-flex align-end">
       <v-btn
