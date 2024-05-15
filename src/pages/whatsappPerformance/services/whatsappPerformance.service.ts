f1ba1ed0ed442d5e53cc1c4edb7926ff7e868f48
guigagb <bsbtrindade@gmail.com>
2024-05-15 11:10:26 -0300

    update endpoint

--- src/pages/whatsappPerformance/services/whatsappPerformance.service.ts
+++ src/pages/whatsappPerformance/services/whatsappPerformance.service.ts
@@ -4,8 +4,8 @@ import { iAtendimentoIniciado, iAtendimentoFinalizado } from "../interfaces";
 type iGetRelatorioAtendimentosIniciadosFunction = (dataInicio: string, dataFim: string) => Promise<iAtendimentoIniciado[]>
 type iGetRelatorioAtendimentosFinalizadosFunction = (dataInicio: string, dataFim: string) => Promise<iAtendimentoFinalizado[]>
 
-const urlServerWhatsapp = "http://localhost:2836"
-// const urlServerWhatsapp = "http://181.215.134.232:2836"
+// const urlServerWhatsapp = "http://localhost:2836"
+const urlServerWhatsapp = "http://181.215.134.232:2836"
 
 const getRelatorioAtendimentosIniciados: iGetRelatorioAtendimentosIniciadosFunction = async (dataInicio, dataFim) => {
     let { data } = await axios.get(`${urlServerWhatsapp}/relatorio-atendimentos-iniciados?dataInicio=${dataInicio}&dataFim=${dataFim}`)
