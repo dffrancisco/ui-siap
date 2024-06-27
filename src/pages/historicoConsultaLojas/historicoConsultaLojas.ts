c7102d20db1050155c0899f954dd1b3e1e1029ad
Vitor Ramos <vitor.rochadf@gmail.com>
2024-06-27 15:53:51 -0300

    init historicoConsultaLojas

---
+++ src/pages/historicoConsultaLojas/historicoConsultaLojas.ts
@@ -0,0 +1,10 @@
+import { reactive } from "vue";
+
+export const state = reactive({
+    loading: false,
+})
+
+export const actions = {
+    async init() {
+    },
+}
---
+++ src/pages/historicoConsultaLojas/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { nextTick } from "vue";
+import { state, actions } from "./historicoConsultaLojas";
+
+nextTick(async () => {
+  actions.init();
+});
+</script>
+
+<template>
+  <v-container>
+    <v-card
+      class="pa-5"
+      style="width: 876px; margin: 0 auto"
+    >
+    </v-card>
+
+    <v-overlay
+      :model-value="state.loading"
+      class="align-center justify-center"
+      persistent
+    >
+      <v-progress-circular
+        color="primary"
+        indeterminate
+        size="64"
+      ></v-progress-circular>
+    </v-overlay>
+
+    <div id="pnCodigoTela">VENDA_PERDIDA</div>
+  </v-container>
+</template>
+
+<style></style>
+
+<style scoped></style>
