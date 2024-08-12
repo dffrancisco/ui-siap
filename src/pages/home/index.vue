8058fbc91a22be0f33869bbb490876af8aded9eb
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-08-12 09:10:22 -0300

    feat: natureza operacao

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -222,6 +222,12 @@ import login from "../login/login";
 
             <v-btn
               class="mr-4 mb-4"
+              @click="router.push('/naturezaOperacao')"
+              >Natureza Operação
+            </v-btn>
+
+            <v-btn
+              class="mr-4 mb-4"
               @click="login.actions.confirmarSaida()"
             >
               Logout
