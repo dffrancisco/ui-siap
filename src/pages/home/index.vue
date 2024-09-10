b5fc5f4d2c434256e01c767dcf1448a153b57e10
Vitor Ramos <vitor.rochadf@gmail.com>
2024-09-10 11:39:39 -0300

    iniciando relatorioConferencia

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -234,6 +234,12 @@ import login from "../login/login";
 
             <v-btn
               class="mr-4 mb-4"
+              @click="router.push('/relatorioConferencia')"
+              >Relatório Conferência
+            </v-btn>
+
+            <v-btn
+              class="mr-4 mb-4"
               @click="login.actions.confirmarSaida()"
             >
               Logout
---
+++ src/pages/relatorioConferencia/index.vue
@@ -0,0 +1,3 @@
+<script setup lang="ts"></script>
+<template><div>relatorioConferencia</div></template>
+<style scoped></style>
