6b8fcd0c355fbf82e85fc48daeac9fbd9213f551
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-09-14 12:57:24 -0300

    feat: grid

--- src/pages/relatorioFaturamento/index.vue
+++ src/pages/relatorioFaturamento/index.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { actions, state } from './relatorioFaturamento';
+
+
+onMounted(()=>{
+  actions.init()
+})
+</script>
 
 <template>
   <v-container>
@@ -26,6 +34,7 @@
           <v-col class="d-flex flex-row align-center ga-2">
             <v-text-field
               label="Cliente (F1)"
+              :disabled="!state.orcamentosFaturados.NOME"
               readonly
             ></v-text-field>
             <v-btn
@@ -35,6 +44,12 @@
             ></v-btn>
           </v-col>
         </v-row>
+      </div>
+
+      <div id="gridOrcamentosFaturados" class="mt-4"></div>
+
+      <div class="mt-2 d-flex justify-end">
+        <v-btn color="primary" size="36" icon="mdi-printer mdi-24px"></v-btn>
       </div>
     </v-card>
   </v-container>
--- src/pages/relatorioFaturamento/relatorioFaturamento.ts
+++ src/pages/relatorioFaturamento/relatorioFaturamento.ts
@@ -1,6 +1,31 @@
+import xGridV2, { ixGridCreate } from "@/plugins/xGridV2";
 import { reactive } from "vue";
-import { ixGrid, ixGridCreate } from "@/plugins/xGridV2";
+import { iOrcamentosClienteFaturado } from "./interfaces";
 
 export const state = reactive({
-    gridOrcamentosFaturados: <ixGridCreate>{}
+    gridOrcamentosFaturados: <ixGridCreate>{},
+    orcamentosFaturados: <iOrcamentosClienteFaturado>{},
+    modalSelecionarClienteFaturadoOpened: false
 })
+
+export const actions = {
+    async init() {
+        actions.createGrid()
+    },
+
+    async createGrid() {
+        state.gridOrcamentosFaturados = new xGridV2.create({
+            el: "#gridOrcamentosFaturados",
+            height: 416,
+            columns: {
+                "N° Fiscal": { dataField: "NUM_NFE" },
+                "N° Orçamento": { dataField: "NUM_ORCAMENTO" },
+                "Data da Saida": { dataField: "DATA" },
+                "Devolução": { dataField: "DEVOLUCAO" },
+                "Desconto": { dataField: "DESCONTO" },
+                "Valor Montagem": { dataField: "MONTAGEM" },
+                "Valor Orçamento": { dataField: "VALOR" },
+            },
+        })
+    }
+}
