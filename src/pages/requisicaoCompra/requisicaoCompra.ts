9f1b3a3e88bae90c0ce92d8ea756a5417ccf1418
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-09-18 16:03:02 -0300

    .

--- src/pages/requisicaoCompra/requisicaoCompra.ts
+++ src/pages/requisicaoCompra/requisicaoCompra.ts
@@ -1,6 +1,5 @@
 import { reactive } from "vue";
 import { iRequisicaoCompra, iRequisicaoItem } from "./interfaces";
-import moment from "moment";
 
 export const state = reactive({
     dbRequisicaoCompra: <iRequisicaoCompra>{},
