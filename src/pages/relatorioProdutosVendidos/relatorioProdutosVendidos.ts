1a6bf1f4d2be9fa4626aff8d0034e5220ccc0a87
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2024-12-13 16:50:00 -0300

    fix per review 2

--- src/pages/relatorioProdutosVendidos/relatorioProdutosVendidos.ts
+++ src/pages/relatorioProdutosVendidos/relatorioProdutosVendidos.ts
@@ -14,7 +14,7 @@ export const state = reactive({
     dataFimImpressao: null,
     totalItems: 0,
     itemsPerPage: 30,
-    page: 0,
+    page: 1,
     dadosRelatorio: <iDadosRelatorioProdutosVendidos[]>[],
     headers: <any>[
         {
