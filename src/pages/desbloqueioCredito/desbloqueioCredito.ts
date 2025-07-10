799112adfe3d6c362c45294c5cf85e221e598d04
PedroThales <pedrothalessp@gmail.com>
2025-07-10 14:48:26 -0300

    loading prontos

--- src/pages/desbloqueioCredito/desbloqueioCredito.ts
+++ src/pages/desbloqueioCredito/desbloqueioCredito.ts
@@ -2,46 +2,41 @@ import Swal from "sweetalert2";
 import { reactive } from "vue";
 import utils from "@/ts/utils";
 
-import { iCredito } from './interfaces'
-import desbloqueioCreditoService from './services/desbloqueioCredito.service';
+import { iCredito } from "./interfaces";
+import desbloqueioCreditoService from "./services/desbloqueioCredito.service";
 
 export const state = reactive({
     credito: <iCredito>{},
     pesquisaCredito: "" as string,
-    loading: false,
-
+    loading: false as boolean,
 });
 
 export const actions = {
-    async init() {
-
-
-
-    },
+    async init() { },
 
     async getCredito(param: string) {
+        state.loading = true;
         if (!state.pesquisaCredito || state.pesquisaCredito.length !== 10) {
             Swal.fire({
-                icon: 'warning',
-                title: 'Chave inválida',
-                text: 'certifique-se de que o campo não está em branco.',
+                icon: "warning",
+                title: "Chave inválida",
+                text: "certifique-se de que o campo não está em branco.",
             });
             return;
         }
 
         try {
             state.credito = await desbloqueioCreditoService.getCredito(param);
-            return
-
+            state.loading = false;
+            return;
         } catch (err: any) {
             if (err.response?.data?.message) {
-                ;
             } else if (err.message) {
             }
 
             Swal.fire({
-                icon: 'info',
-                title: 'Esse crédito não está bloqueado',
+                icon: "info",
+                title: "Esse crédito não está bloqueado",
             });
         }
     },
@@ -49,33 +44,27 @@ export const actions = {
     onClickConfirmaDesbloqueio() {
         utils.confirmaCodigo({
             msg: `Deseja liberar o crédito ${state.credito.CHAVE}?`,
-            theme: 'xModal-bublue',
+            theme: "xModal-bublue",
             call: async () => {
-
                 try {
-                    state.loading = true
+                    state.loading = true;
                     await desbloqueioCreditoService.updateCredito(state.credito.CHAVE);
 
                     Swal.fire({
-                        icon: 'success',
-                        title: 'Crédito desbloqueado',
+                        icon: "success",
+                        title: "Crédito desbloqueado",
                     });
-                    state.pesquisaCredito = ""
-                    state.credito = {} as iCredito
-
+                    state.pesquisaCredito = "";
+                    state.credito = {} as iCredito;
                 } catch (error) {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Erro ao processar sua solicitação.',
+                        icon: "error",
+                        title: "Erro ao processar sua solicitação.",
                     });
                 } finally {
-                    state.loading = false
-                    console.log(state.loading)
+                    state.loading = false;
                 }
-            }
-        })
-    }
-
+            },
+        });
+    },
 };
-
-
