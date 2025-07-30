e2af7a337eabf000ada4b8e5ea1ebca9c179ea27
PedroThales <pedrothalessp@gmail.com>
2025-07-30 16:01:35 -0300

    criando src

--- src/pages/devolucaoManualCaixa/index.vue
+++ src/pages/devolucaoManualCaixa/index.vue
@@ -23,10 +23,11 @@ onMounted(() => {
           <v-avatar size="60">
             <img
               class="w-100"
-              src="https://www.reallatas.com.br/foto_funcionarios/06664612100.jpg"
+              src="`https://www.reallatas.com.br/foto_funcionarios/${funcionario.CPF}.jpg`"
             />
+            <!-- src="`https://www.reallatas.com.br/foto_funcionarios/${funcionario.CPF}.jpg`" -->
           </v-avatar>
-          <span class="text-h6"> {{ caixa.USUARIO }}</span>
+          <span class="font-weight-medium text-h6"> {{ caixa.USUARIO }}</span>
         </v-card>
       </v-col>
     </v-row>
