2e2233ee7175fd0b3213bab7a5bac2d8bf1e0f0e
neilsonfisica <neilson.lopes.sousa@gmail.com>
2025-08-27 17:50:20 -0300

    2° correções 27/08

--- src/pages/telaCPD/interfaces.ts
+++ src/pages/telaCPD/interfaces.ts
@@ -3,3 +3,9 @@ export interface iGetLojasResponse {
     ID_EMPRESA: number;
     FANTASIA: string;
 };
+export interface iLoja {
+    ID_EMPRESA: number;
+    FANTASIA: string;
+    loja?: string;
+}
+
